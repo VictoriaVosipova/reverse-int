12a80027fa2cb8ffc47534c4675f88049a78a161
Victoria Vosipova <crank-line@rambler.ru>
2020-10-02 20:01:09 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+  let str = String(n);
+
+  let arr = str.split('');
+  if (arr[0] === '-') {
+    arr.shift();
+  }
+
+  let newArray = arr.reverse();
+  return +newArray.join('');
+
 }
